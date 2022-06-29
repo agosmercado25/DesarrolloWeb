b314a58508c143ec40111b42ca6e8ec199505320
Agostina <agostinamercado1999@gmail.com>
2022-06-29 02:24:18 -0300

    Ejercicios JavaScript

---
+++ JavaScript/Ejercicios/ejercicio3.js
@@ -0,0 +1,7 @@
+function media(val1, val2, val3){
+  media = ((val1 + val2 + val3) / 3);
+  return media;
+}
+
+resultado = media(1,5,8);
+console.log("La media entre los 3 números es: " + resultado);
---
+++ JavaScript/Ejercicios/ejercicio5.js
@@ -0,0 +1,10 @@
+var persona = {nombre: "Agostina",
+               apellido: "Mercado",
+               edad: 23,
+               info: function(){
+                 console.log("Persona [Nombre: " + this.nombre + ", Apellido: " + this.apellido + ", Edad " + this.edad);
+                 return datos = "Persona [Nombre: " + this.nombre + ", Apellido: " + this.apellido + ", Edad " + this.edad
+               }
+              }
+
+console.log(persona.info());
